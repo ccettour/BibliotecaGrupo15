6cee56dd490371299e3a8ef4d39967804a59d4d0
Juan Castro <juanboffi44@gmail.com>
2023-10-03 15:51:47 -0300

    prestamo y ejemplar

--- src/bibliotecagrupo15/entidades/Ejemplar.java
+++ src/bibliotecagrupo15/entidades/Ejemplar.java
@@ -1,5 +1,41 @@
 package bibliotecagrupo15.entidades;
 
 public class Ejemplar {
-    
+ int codigo;
+ Libro libro;
+ boolean estado;
+
+    public Ejemplar(int codigo, Libro libro, boolean estado) {
+        this.codigo = codigo;
+        this.libro = libro;
+        this.estado = estado;
+    }
+
+    public Ejemplar() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+ 
 }
--- src/bibliotecagrupo15/entidades/Prestamo.java
+++ src/bibliotecagrupo15/entidades/Prestamo.java
@@ -1,5 +1,71 @@
 package bibliotecagrupo15.entidades;
 
+import java.time.LocalDate;
+
+
 public class Prestamo {
+ LocalDate fechaInicio;
+ LocalDate fechaFin;
+ Ejemplar ejemplar;
+ Lector lector;
+ boolean estado;
+
+    public Prestamo() {
+    }
+
+    public Prestamo(LocalDate fechaInicio, LocalDate fechaFin, Ejemplar ejemplar, Lector lector, boolean estado) {
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.ejemplar = ejemplar;
+        this.lector = lector;
+        this.estado = estado;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Ejemplar getEjemplar() {
+        return ejemplar;
+    }
+
+    public void setEjemplar(Ejemplar ejemplar) {
+        this.ejemplar = ejemplar;
+    }
+
+    public Lector getLector() {
+        return lector;
+    }
+
+    public void setLector(Lector lector) {
+        this.lector = lector;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+ 
+    public void solicitarLibro(Ejemplar a, Lector i){
+    estado=true;
+    }
     
+    public void devolverLibro(Ejemplar a, Lector i){
+    estado=false;
+    }
 }
