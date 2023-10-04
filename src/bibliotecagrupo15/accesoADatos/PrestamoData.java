a09cc8197fc1c85b8318c0a29806a78d53829cb7
Juan Castro <juanboffi44@gmail.com>
2023-10-04 16:35:36 -0300

    cambios a prestamoData

--- src/bibliotecagrupo15/accesoADatos/PrestamoData.java
+++ src/bibliotecagrupo15/accesoADatos/PrestamoData.java
@@ -1,6 +1,6 @@
 package bibliotecagrupo15.accesoADatos;
 
-import bibliotecagrupo15.entidades.Ejemplar;
+import bibliotecagrupo15.entidades.Libro;
 import bibliotecagrupo15.entidades.Lector;
 import bibliotecagrupo15.entidades.Prestamo;
 import java.sql.*;
@@ -12,82 +12,92 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class PrestamoData {
-     private Connection con = null;
-     private LectorData ld= new LectorData();
-     private EjemplarData ed= new EjemplarData();
-     
-     public PrestamoData(){
-     con = Conexion.getConexion();
-     }
-     
-     public void crearPrestamo(Prestamo pres){
-     String sql="INSERT INTO `prestamo`(fechaInicio, fechaFin, estado, idEjemplar , idLector) "
-             + "VALUES (?,?,?)";
-     
-         try {
-             PreparedStatement ps=con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-             ps.setDate(1, Date.valueOf(pres.getFechaInicio()));
-             ps.setDate(2, Date.valueOf(pres.getFechaFin()));
-             ps.setBoolean(3, true);
-             ps.executeUpdate();
-             ResultSet rs=ps.getGeneratedKeys();
-             if (rs.next()) {
+
+    private Connection con = null;
+    private LectorData ld = new LectorData();
+    private EjemplarData ed = new EjemplarData();
+
+    public PrestamoData() {
+        con = Conexion.getConexion();
+    }
+
+    public void crearPrestamo(Prestamo pres) {
+        String sql = "INSERT INTO `prestamo`(fechaInicio, fechaFin, estado, idEjemplar , idLector) "
+                + "VALUES (?,?,?,?,?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setDate(1, Date.valueOf(pres.getFechaInicio()));
+            ps.setDate(2, Date.valueOf(pres.getFechaFin()));
+            ps.setBoolean(3, true);
+            ps.setInt(4, pres.getEjemplar().getIdEjemplar());
+            ps.setInt(5, pres.getLector());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
                 pres.setIdPrestamo(rs.getInt(1));
-                 JOptionPane.showMessageDialog(null, "Prestamo creado con exito");
-             }
-             ps.close();
-             
-         } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Prestamo creado con exito");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla de prestamos");
-         }
-     
-     }
-     
-    
-     
-     public List<Lector> ListarLectoresxFecha(LocalDate fecha, int idLector){
-     ArrayList<Lector> lectores=new ArrayList<>();
-     String sql="SELECT lec.idLector, nroSocio, lec.nombre, mail "
-             + "FROM `lector` AS lec JOIN prestamo "
-             + "ON lec.idLector=prestamo.idLector WHERE prestamo.idPrestamo=?";
-         try {
-              PreparedStatement ps= con.prepareStatement(sql);
-              ps.setInt(1, idLector);
-              ResultSet rs=ps.executeQuery();
-             if (fecha.isAfter(rs.getDate("fechaFin").toLocalDate())) {
-                 while (rs.next()) {                     
-                     Lector lector= new Lector();
-                     
-                 }
-   
-             }
-             
-             
-             
-         } catch (SQLException ex) {
-             Logger.getLogger(PrestamoData.class.getName()).log(Level.SEVERE, null, ex);
-         }
-     
-     
-     }
-     
-     
-     public List<Ejemplar> ListarLibrosxFecha(LocalDate fecha, int idEjemplar){
-     ArrayList<Ejemplar> ejemplares=new ArrayList<>();
-     String sql="";
-         try {
-              PreparedStatement ps= con.prepareStatement(sql);
-              ps.setInt(1, idEjemplar);
-              ResultSet rs=ps.executeQuery();
-             
-             
-             
-             
-         } catch (SQLException ex) {
-             Logger.getLogger(PrestamoData.class.getName()).log(Level.SEVERE, null, ex);
-         }
-     
-     
-     }
-     
+        }
+
+    }
+
+//    public List<Lector> ListarLectoresxFecha(LocalDate fecha, int idLector) {
+//        ArrayList<Lector> lectores = new ArrayList<>();
+//        String sql = "SELECT lec.idLector, nroSocio, lec.nombre, mail "
+//                + "FROM `lector` AS lec JOIN prestamo "
+//                + "ON lec.idLector=prestamo.idLector WHERE prestamo.idPrestamo=?";
+//        try {
+//            PreparedStatement ps = con.prepareStatement(sql);
+//            ps.setInt(1, idLector);
+//            ResultSet rs = ps.executeQuery();
+//            if (fecha.isAfter(rs.getDate("fechaFin").toLocalDate())) {
+//                while (rs.next()) {
+//                    Lector lector = new Lector();
+//
+//                }
+//
+//            }
+//
+//        } catch (SQLException ex) {
+//           JOptionPane.showMessageDialog(null, "Error al acceder a la tabla de prestamos");
+//        }
+//
+//    }
+
+    public List<Libro> ListarLibrosxFecha(Date fecha, int idEjemplar) {
+        ArrayList<Libro> libros = new ArrayList<>();
+        String sql = "SELECT lib.titulo, lib.isbn, lib.editorial, lib.anio "
+                + "FROM `libro` AS lib "
+                + "JOIN ejemplar ON ejemplar.idLibro=lib.idLibro "
+                + "JOIN prestamo ON prestamo.idEjemplar = ejemplar.idEjemplar"
+                + "WHERE prestamo.fechaInicio=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setDate(1, fecha);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {                
+              Libro libro=new Libro();  
+              libro.setIdLibro(rs.getInt("idLibro"));
+              libro.setIsbn(rs.getInt("isbn"));
+              libro.setEditorial(rs.getString("editorial"));
+              libro.setAnio(rs.getInt("anio"));
+              libro.setTitulo(rs.getString("titulo"));
+              libro.setEstado(rs.getBoolean("estado"));
+              libros.add(libro);
+              
+            }
+            
+            ps.close();
+        
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla de prestamos");
+        }
+           return libros;
+    }
+
 }
--- src/bibliotecagrupo15/entidades/Ejemplar.java
+++ src/bibliotecagrupo15/entidades/Ejemplar.java
@@ -1,9 +1,17 @@
 package bibliotecagrupo15.entidades;
 
 public class Ejemplar {
+ int idEjemplar;   
  int codigo;
  Libro libro;
  boolean estado;
+
+    public Ejemplar(int idEjemplar, int codigo, Libro libro, boolean estado) {
+        this.idEjemplar = idEjemplar;
+        this.codigo = codigo;
+        this.libro = libro;
+        this.estado = estado;
+    }
 
     public Ejemplar(int codigo, Libro libro, boolean estado) {
         this.codigo = codigo;
@@ -12,6 +20,14 @@ public class Ejemplar {
     }
 
     public Ejemplar() {
+    }
+
+    public int getIdEjemplar() {
+        return idEjemplar;
+    }
+
+    public void setIdEjemplar(int idEjemplar) {
+        this.idEjemplar = idEjemplar;
     }
 
     public int getCodigo() {
