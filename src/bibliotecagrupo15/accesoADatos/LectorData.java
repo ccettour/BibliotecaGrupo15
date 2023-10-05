941acbd65e01413873f293ef4bb66cb1a3987d5b
Juan Castro <juanboffi44@gmail.com>
2023-10-05 15:50:54 -0300

    agregado metodos en lectorData

--- src/bibliotecagrupo15/accesoADatos/LectorData.java
+++ src/bibliotecagrupo15/accesoADatos/LectorData.java
@@ -1,11 +1,13 @@
 package bibliotecagrupo15.accesoADatos;
 
 import bibliotecagrupo15.entidades.Lector;
+import java.util.List;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class LectorData {
@@ -40,7 +42,7 @@ public class LectorData {
 
         }
     }
-    
+
     public Lector buscarLector(int idLector) {
         Lector lector = null;
         String sql = "SELECT nroSocio,nombre,domicilio,mail FROM lector WHERE idLector=? AND estado=1";
@@ -57,7 +59,7 @@ public class LectorData {
                 lector.setNombre("nombre");
                 lector.setMail("mail");
                 lector.setEstado(true);
-                JOptionPane.showMessageDialog(null,"lector encontrado");
+                JOptionPane.showMessageDialog(null, "lector encontrado");
             } else {
                 JOptionPane.showMessageDialog(null, "No existe el lector");
             }
@@ -72,9 +74,8 @@ public class LectorData {
 
     public void modificarLector(Lector lector) {
         String sql = "UPDATE lector SET nombre=?,domicilio=?,mail=?,estado=? WHERE nroSocio=?";
-        PreparedStatement ps = null;
         try {
-
+            PreparedStatement ps = con.prepareStatement(sql);
             ps.setString(1, lector.getNombre());
             ps.setString(2, lector.getDomicilio());
             ps.setString(3, lector.getMail());
@@ -90,16 +91,15 @@ public class LectorData {
             }
         } catch (SQLException x) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla" + x.getMessage());
-
         }
 
     }
 
     public void eliminarLector(Lector lector) {
+        String sql = "UPDATE lector SET estado=0 WHERE nroSocio=?";
         try {
-            String sql = "UPDATE lector SET estado=0 WHERE nroSocio=?";
             PreparedStatement ps = con.prepareStatement(sql);
-            ps.setInt(1,lector.getSocio());
+            ps.setInt(1, lector.getSocio());
             int fila = ps.executeUpdate();
 
             if (fila == 1) {
@@ -111,6 +111,29 @@ public class LectorData {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla" + x.getMessage());
 
         }
+
+    }
+
+    public List<Lector> listarLectores() {
+        String sql = "SELECT* FROM lector WHERE estado=1";
+        ArrayList<Lector> lectores = new ArrayList<>();
+        try {
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Lector lector = new Lector();
+                lector.setIdLector(rs.getInt("idLector"));
+                lector.setSocio(rs.getInt("nroSocio"));
+                lector.setNombre("nombre");
+                lector.setMail("mail");
+                lector.setEstado(true);
+                lectores.add(lector);
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla de alumnos");
+        }
+        return lectores;
 
     }
 }
