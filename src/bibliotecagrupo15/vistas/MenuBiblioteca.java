0aa56ebb680a5fc9c2097075db1721ab3e881232
Juan Castro <juanboffi44@gmail.com>
2023-10-12 16:57:29 -0300

    adminAutores

--- src/bibliotecagrupo15/vistas/MenuBiblioteca.java
+++ src/bibliotecagrupo15/vistas/MenuBiblioteca.java
@@ -42,7 +42,7 @@ public class MenuBiblioteca extends javax.swing.JFrame {
         listadoDeSocios = new javax.swing.JMenuItem();
         listaDeLibros = new javax.swing.JMenuItem();
         listadoDePrestamos = new javax.swing.JMenuItem();
-        jMenuItem1 = new javax.swing.JMenuItem();
+        adminAutores = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -112,13 +112,13 @@ public class MenuBiblioteca extends javax.swing.JFrame {
         listadoDePrestamos.setText("Listado de Prestamos");
         jMenu3.add(listadoDePrestamos);
 
-        jMenuItem1.setText("Administracion de autores");
-        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+        adminAutores.setText("Administracion de autores");
+        adminAutores.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem1ActionPerformed(evt);
+                adminAutoresActionPerformed(evt);
             }
         });
-        jMenu3.add(jMenuItem1);
+        jMenu3.add(adminAutores);
 
         jMenuBar1.add(jMenu3);
 
@@ -149,9 +149,9 @@ public class MenuBiblioteca extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_pedirPrestamoActionPerformed
 
-    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+    private void adminAutoresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_adminAutoresActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem1ActionPerformed
+    }//GEN-LAST:event_adminAutoresActionPerformed
 
     /**
      * @param args the command line arguments
@@ -189,6 +189,7 @@ public class MenuBiblioteca extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem adminAutores;
     private javax.swing.JMenuItem agregarEjemplares;
     private javax.swing.JMenuItem agregarLibro;
     private javax.swing.JMenuItem devolver;
@@ -199,7 +200,6 @@ public class MenuBiblioteca extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem listaDeLibros;
     private javax.swing.JMenuItem listadoDePrestamos;
     private javax.swing.JMenuItem listadoDeSocios;
