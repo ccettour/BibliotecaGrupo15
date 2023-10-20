a12b980cee43537a6973114e6473d80cf804757d
Juan Castro <juanboffi44@gmail.com>
2023-10-20 17:54:44 -0300

    Tipo agregado

---
+++ src/bibliotecagrupo15/Tipo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package bibliotecagrupo15;
+
+/**
+ *
+ * @author Chris
+ */
+public enum Tipo {
+    INFORMÁTICA, CIENCIAS, LITERATURA, INFANTIL, DERECHO, ECONOMÍA, OTRO
+}
