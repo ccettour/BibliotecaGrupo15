b9c16d76aef888a01283653b7722ea23486730ba
Christian <christiancettour@gmail.com>
2023-10-26 16:17:50 -0300

    Tratamiento excepción en inicio

--- src/bibliotecagrupo15/accesoADatos/Conexion.java
+++ src/bibliotecagrupo15/accesoADatos/Conexion.java
@@ -5,30 +5,30 @@ import java.sql.*;
 import javax.swing.JOptionPane;
 
 public class Conexion {
-    private static final String URL="jdbc:mariadb://localhost/";
-    private static final String DB="biblioteca";
-    private static final String USUARIO="root";
-    private static final String PASSWORD="";
+
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "biblioteca";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
     private static Connection connection;
-    
-    private Conexion(){}
-    
-    public static Connection getConexion(){
-    if(connection==null){
-    
-        try {
-            Class.forName("org.mariadb.jdbc.Driver");
-            connection=DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
-            
-        } catch (ClassNotFoundException ex) {
-          JOptionPane.showMessageDialog(null, "Error al cargar los drivers");
-        } catch (SQLException ex) {
-          JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos");
-        } catch( java.lang.NullPointerException j){
-            System.out.println("Por favor conectarse a la base de datos");
-        }
+
+    private Conexion() {
     }
+
+    public static Connection getConexion() {
+        if (connection == null) {
+
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL + DB, USUARIO, PASSWORD);
+
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar los drivers");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos");
+            }
+        }
         return connection;
-        
+
     }
 }
